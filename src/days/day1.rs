390952cf71a4723c5b30d77fa4d539aa7a3ddd9f
Rhys Parry <rhys@i-think22.net>
2023-12-02 13:51:39 +1000

    Reusable Solution struct

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,19 +1,7 @@
 use crate::io::Source;
 use crate::trebuchet::{sum_calibration_values, sum_calibration_values_v2};
+use crate::Solution;
 use std::error::Error;
-use std::fmt;
-use std::fmt::{Display, Formatter};
-
-struct Solution {
-    part1: i32,
-    part2: i32,
-}
-
-impl Display for Solution {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "part 1: {}\npart 2: {}", self.part1, self.part2)
-    }
-}
 
 pub fn run(input: &Source) -> Result<(), Box<dyn Error>> {
     let solution = solve(input)?;
@@ -24,10 +12,10 @@ pub fn run(input: &Source) -> Result<(), Box<dyn Error>> {
 fn solve(input: &Source) -> Result<Solution, Box<dyn Error>> {
     let input = input.read_string()?;
 
-    Ok(Solution {
-        part1: sum_calibration_values(&input)?,
-        part2: sum_calibration_values_v2(&input)?,
-    })
+    Ok(Solution::new(
+        sum_calibration_values(&input)?,
+        sum_calibration_values_v2(&input)?,
+    ))
 }
 
 #[cfg(test)]
@@ -38,7 +26,7 @@ mod tests {
     fn test_solve_part_1() -> Result<(), Box<dyn Error>> {
         let input = Source::try_from("inputs/day-1.txt")?;
         let result = solve(&input)?;
-        assert_eq!(result.part1, 55029);
+        assert_eq!(result.part1(), 55029);
         Ok(())
     }
 
@@ -46,7 +34,7 @@ mod tests {
     fn test_solve_part_2() -> Result<(), Box<dyn Error>> {
         let input = Source::try_from("inputs/day-1.txt")?;
         let result = solve(&input)?;
-        assert_eq!(result.part2, 55686);
+        assert_eq!(result.part2(), 55686);
         Ok(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,31 @@
+use std::fmt;
+use std::fmt::{Display, Formatter};
+
 pub mod io;
 
 pub mod days;
 pub mod trebuchet;
+
+pub struct Solution {
+    part1: i32,
+    part2: i32,
+}
+
+impl Solution {
+    pub fn new(part1: i32, part2: i32) -> Self {
+        Solution { part1, part2 }
+    }
+
+    pub fn part1(&self) -> i32 {
+        self.part1
+    }
+    pub fn part2(&self) -> i32 {
+        self.part2
+    }
+}
+
+impl Display for Solution {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "part 1: {}\npart 2: {}", self.part1, self.part2)
+    }
+}
