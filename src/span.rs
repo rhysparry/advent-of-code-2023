af34e56199e519041e04ccb91023e44be02bfe6a
Rhys Parry <rhys@i-think22.net>
2023-12-04 13:35:38 +1000

    Add span tests

--- src/span.rs
+++ src/span.rs
@@ -28,3 +28,64 @@ where
         self.start < other.end && other.start < self.end
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_left_adjacent_to() {
+        let a = 1..3;
+        let b = 3..5;
+        assert!(a.is_left_adjacent_to(&b));
+        assert!(!b.is_left_adjacent_to(&a));
+    }
+
+    #[test]
+    fn test_is_right_adjacent_to() {
+        let a = 1..3;
+        let b = 3..5;
+        assert!(!a.is_right_adjacent_to(&b));
+        assert!(b.is_right_adjacent_to(&a));
+    }
+
+    #[test]
+    fn test_overlaps_negative() {
+        let a = 1..3;
+        let b = 3..5;
+        assert!(!a.overlaps(&b));
+        assert!(!b.overlaps(&a));
+    }
+
+    #[test]
+    fn test_overlaps() {
+        let a = 1..4;
+        let b = 3..5;
+        assert!(a.overlaps(&b));
+        assert!(b.overlaps(&a));
+    }
+
+    #[test]
+    fn test_is_adjacent_to() {
+        let a = 1..3;
+        let b = 3..5;
+        assert!(a.is_adjacent_to(&b));
+        assert!(b.is_adjacent_to(&a));
+    }
+
+    #[test]
+    fn test_overlaps_or_is_adjacent_to() {
+        let a = 1..3;
+        let b = 3..5;
+        assert!(a.overlaps_or_is_adjacent_to(&b));
+        assert!(b.overlaps_or_is_adjacent_to(&a));
+    }
+
+    #[test]
+    fn test_overlaps_or_is_adjacent_to_negative() {
+        let a = 1..2;
+        let b = 3..5;
+        assert!(!a.overlaps_or_is_adjacent_to(&b));
+        assert!(!b.overlaps_or_is_adjacent_to(&a));
+    }
+}
